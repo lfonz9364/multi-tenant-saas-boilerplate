ffcd86f05731a8a0aba352c271a5145e619c40a8
Alfons Caroles <cllin7787@gmail.com>
2025-07-15 19:16:37 +1000

    Moved clerk buttons to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,29 +1,44 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function DashboardPage() {
+export default function Dashboard() {
   const { user, isLoaded } = useUser();
   const router = useRouter();
+
+  const userButtonAppearance = {
+    elements: {
+      userButtonAvatarBox: {
+        width: "8rem",
+        height: "8rem",
+      },
+    },
+  };
 
   useEffect(() => {
     if (isLoaded && user) {
       const onboardUser = async () => {
         try {
-          const res = await fetch("api/onboard", {
-            method: "POST",
-            body: JSON.stringify({
-              clerkUserId: user.id,
-              email: user.primaryEmailAddress?.emailAddress,
-              name: user.fullName,
-            }),
-          });
-          const data: { tenantSlug: string } = await res.json();
-          if (data.tenantSlug) {
-            router.push(`/dashboard/${data.tenantSlug}`);
-          }
+          // const res = await fetch("api/onboard", {
+          //   method: "POST",
+          //   body: JSON.stringify({
+          //     clerkUserId: user.id,
+          //     email: user.primaryEmailAddress?.emailAddress,
+          //     name: user.fullName,
+          //   }),
+          // });
+          // const data: { tenantSlug: string } = await res.json();
+          // if (data.tenantSlug) {
+          //   router.push(`/dashboard/${data.tenantSlug}`);
+          // }
         } catch (error) {
           throw new Error("Error onboarding user: " + (error as Error).message);
         }
@@ -34,7 +49,16 @@ export default function DashboardPage() {
   }, [isLoaded, router, user]);
 
   return (
-    <div className="w-full flex items-center justify-center">
+    <div className="w-full flex flex-col items-center justify-center">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <span className="mb-8 flex flex-col items-center justify-between">
+          <UserButton appearance={userButtonAppearance} />
+          <p className="mt-4">Welcome to the SaaS starter ✨</p>
+        </span>
+      </SignedIn>
       <p>Redirecting to your dashboard...</p>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import DashboardPage from "./dashboard/page";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
-  const userButtonAppearance = {
-    elements: {
-      userButtonAvatarBox: {
-        width: "8rem",
-        height: "8rem",
-      },
-    },
-  };
-
   return (
     <main className="p-10 fixed inset-0 flex flex-col items-center justify-center">
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <span className="mb-8 flex flex-col items-center justify-between">
-          <UserButton appearance={userButtonAppearance} />
-          <p className="mt-4">Welcome to the SaaS starter ✨</p>
-        </span>
-      </SignedIn>
-      <DashboardPage />
+      <Dashboard />
     </main>
   );
 }
