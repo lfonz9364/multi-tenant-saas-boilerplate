743d97b607f0ef8d005fadf3091a1f98fc69af04
Alfons Caroles <cllin7787@gmail.com>
2025-07-15 19:11:57 +1000

    Styling landing page after login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,25 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import DashboardPage from "./dashboard/page";
 
 export default function Home() {
+  const userButtonAppearance = {
+    elements: {
+      userButtonAvatarBox: {
+        width: "8rem",
+        height: "8rem",
+      },
+    },
+  };
+
   return (
-    <main className="p-10">
+    <main className="p-10 fixed inset-0 flex flex-col items-center justify-center">
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
-        <UserButton />
-        <p>Welcome to the SaaS starter ✨</p>
+        <span className="mb-8 flex flex-col items-center justify-between">
+          <UserButton appearance={userButtonAppearance} />
+          <p className="mt-4">Welcome to the SaaS starter ✨</p>
+        </span>
       </SignedIn>
       <DashboardPage />
     </main>
