ed8bf6b4be72a37ee40b12b5dab8f77de1e88d21
Alfons Caroles <cllin7787@gmail.com>
2025-07-23 14:38:28 +1000

    Updated edit and delete user to use Supabase client as per Supabase security advise

--- app/api/clerk/user/route.ts
+++ app/api/clerk/user/route.ts
@@ -1,26 +1,32 @@
 import { clerkAPI } from "@/lib/clerk";
-import { prisma } from "@/lib/db";
 import { logAudit } from "@/lib/logging";
+import { createSupabaseClient } from "@/lib/supabaseClient";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(req: NextRequest) {
   const { userId: currentUserId } = await auth();
   const { userId } = await req.json();
+  const supabase = await createSupabaseClient();
 
   try {
     // Delete from Clerk
     await clerkAPI.delete(`users/${userId}`);
 
     // Delete record from Supabase
-    await prisma.$transaction([
-      prisma.membership.deleteMany({
-        where: { userId },
-      }),
-      prisma.user.delete({
-        where: { id: userId },
-      }),
-    ]);
+    const { error: deleteMembershipError } = await supabase
+      .from("Membership")
+      .delete()
+      .eq("userId", userId);
+
+    const { error: deleteUserError } = await supabase
+      .from("User")
+      .delete()
+      .eq("id", userId);
+
+    if (deleteMembershipError || deleteUserError) {
+      throw new Error("Supabase deletion failed");
+    }
 
     await logAudit({
       action: "DELETE_USER",
@@ -42,16 +48,21 @@ export async function DELETE(req: NextRequest) {
 export async function PATCH(req: NextRequest) {
   const { userId: currentUserId } = await auth();
   const { userId, updateData } = await req.json();
+  const supabase = await createSupabaseClient();
 
   try {
     //  Update in Clerk
     const res = await clerkAPI.patch(`users/${userId}`, updateData);
 
     // Update in Supabase
-    const updatedUser = await prisma.user.update({
-      where: { id: userId },
-      data: updateData,
-    });
+    const { data: updatedUser, error } = await supabase
+      .from("User")
+      .update(updateData)
+      .eq("id", userId)
+      .select()
+      .single();
+
+    if (error) throw error;
 
     await logAudit({
       action: "UPDATE_USER",
